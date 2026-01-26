31913842ec6e9467008b12d158ce65c38fae9625
Pedro Onayquem <ponayquen@gmail.com>
2026-01-26 10:44:42 -0300

    feat: adiciona exceção ResourceNotFoundException

--- src/main/java/com/onayquem/taskmanager/exception/ResourceNotFoundException.java
+++ src/main/java/com/onayquem/taskmanager/exception/ResourceNotFoundException.java
@@ -2,6 +2,7 @@ package com.onayquem.taskmanager.exception;
 
 public class ResourceNotFoundException extends RuntimeException{
     public ResourceNotFoundException(String message) {
+
         super(message);
     }
 }
