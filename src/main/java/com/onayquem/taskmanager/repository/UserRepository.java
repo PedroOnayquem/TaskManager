22c14c3f10cdcd59a341e1011f12704c5f3c8cb5
Pedro Onayquem <ponayquen@gmail.com>
2026-02-05 15:59:04 -0300

    feat: adiciona entidade User com UserDetails e repositório

---
+++ src/main/java/com/onayquem/taskmanager/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.onayquem.taskmanager.repository;
+
+import com.onayquem.taskmanager.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
